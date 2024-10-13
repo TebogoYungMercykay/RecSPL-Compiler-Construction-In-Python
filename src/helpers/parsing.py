8928b21160318ea9c136762b89d7cf925301bf96
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-13 13:17:09 +0200

    (helpers) Parser Caller Function

---
+++ src/helpers/parsing.py
@@ -0,0 +1,32 @@
+from parser import Parser
+from src.utilities.xml_methods import (
+    write_to_file,
+    parse_xml_tokens,
+    convert_syntax_tree_to_pretty_xml,
+)
+
+def parsing(lexer_filepath, parser_filepath):
+    print("-- Parsing The RecSPL Code --")
+    print("----------------------------------")
+
+    tokens = parse_xml_tokens(lexer_filepath)
+    parser = Parser(tokens)
+
+    try:
+        parser = Parser(tokens)
+        syntax_tree = parser.parse()
+
+        pretty_xml = convert_syntax_tree_to_pretty_xml(syntax_tree)
+        write_to_file(pretty_xml, parser_filepath)
+
+        print(f"Syntax Tree Generated and Saved to {parser_filepath}")
+        print("Parsing successful.")
+        print("----------------------------------\n\n")
+
+        return True
+
+    except SyntaxError as e:
+        print(f"Parsing failed: {e}")
+        print("----------------------------------\n\n")
+
+        return False
