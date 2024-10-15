82677b59df81adb19a6e1b66f5d622f5f23c2dc7
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-16 01:32:59 +0200

    Added HighLighters for Success/Failure Messages

--- src/main.py
+++ src/main.py
@@ -17,28 +17,45 @@ parser_filepath = "out/syntax_tree.xml"
 crawling_filepath = "out/semantics_crawling_output.txt"
 semantics_filepath = "out/semantics_sybols_output.txt"
 
-LEXING_SUCCESSFUL = lexing(lexer_filepath, code_filename)
-if not LEXING_SUCCESSFUL:
-    print("Parsing Paused:  Please Fix Lexical Errors To Continue...")
-
-PARSING_SUCCESSFUL = (
-    parsing(lexer_filepath, parser_filepath) if LEXING_SUCCESSFUL else False
-)
-if not PARSING_SUCCESSFUL:
+def test():
+    LEXING_SUCCESSFUL = lexing(lexer_filepath, code_filename)
     if not LEXING_SUCCESSFUL:
-        print()
-    else:
-        print("Type Checker Paused:  Please Check for Syntax Errors...")
+        print(
+            "\033[91mParsing Paused:  Please Fix Lexical Errors To Continue...\033[0m"
+        )
 
-TYPE_SUCCESSFUL = (
-    analyser(parser_filepath, crawling_filepath, semantics_filepath)
-    if PARSING_SUCCESSFUL
-    else False
-)
-if not TYPE_SUCCESSFUL:
+    PARSING_SUCCESSFUL = (
+        parsing(lexer_filepath, parser_filepath) if LEXING_SUCCESSFUL else False
+    )
     if not PARSING_SUCCESSFUL:
-        print()
+        if not LEXING_SUCCESSFUL:
+            print()
+        else:
+            print(
+                "\033[91mType Checker Paused:  Please Check for Syntax Errors...\033[0m"
+            )
+
+    TYPE_SUCCESSFUL = (
+        analyser(parser_filepath, crawling_filepath, semantics_filepath)
+        if PARSING_SUCCESSFUL
+        else False
+    )
+    if not TYPE_SUCCESSFUL:
+        if not PARSING_SUCCESSFUL:
+            print()
+        else:
+            print(
+                "\033[91mSemantic Analysis Paused:  Please Check for Static Scoping and Typing Errors...\033[0m"
+            )
+    
+    if (TYPE_SUCCESSFUL): 
+        print(
+            "\033[92mTesting Complete!.\033[0m"
+        )
     else:
         print(
-            "Semantic Analysis Paused:  Please Check for Static Scoping and Typing Errors..."
+            "\033[91mSome Tests Were Unsuccessful!.\033[0m"
         )
+    return True
+
+test()
