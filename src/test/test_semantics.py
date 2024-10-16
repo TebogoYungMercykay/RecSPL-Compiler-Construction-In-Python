8dfe07eaeedfdce2063c16f0124f2124a8d79a6f
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-16 04:43:28 +0200

    Updated Test Case To get Rid Of a Warning

--- src/test/test_semantics.py
+++ src/test/test_semantics.py
@@ -1,12 +1,10 @@
 import pytest
-from helpers.lexing import lexing
-from helpers.parsing import parsing
 
 def test_pytest_is_working_1():
-    assert True, "Semantics Default Tests Working"
+    assert 1 + 1 == 2, "Semantics Default Tests Working"
 
 def test_semantics_from_files():
-    assert True, "Semantics - All Good"
+    assert 1 + 1 == 2, "Semantics - All Good"
 
 if __name__ == "__main__":
     # pytest -v
