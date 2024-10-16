878ee7362498a98562cff7b64418241db83c8d1b
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-16 04:35:35 +0200

    (tests) Using Paramerised Tests for The Lexer

--- src/test/test_lexer.py
+++ src/test/test_lexer.py
@@ -8,7 +8,7 @@ def test_lexing_files_1_to_5(file_number):
 @pytest.mark.parametrize("file_number", range(6, 21))
 def test_lexing_files_6_to_20(file_number):
     assert lexing(f"out/testing/tokens/lexer-{file_number}.xml", f"out/testing/recspl/code-{file_number}.txt") == False, \
-        f"Lexing Should Not Pass For RecSPL code-{file_number}.txt"
+        f"Lexing Should Not Pass For this RecSPL code-{file_number}.txt"
 
 if __name__ == "__main__":
     # pytest -v
