2cd9bdff79607d2b7e3e8de969f62a0cb9170e2c
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-16 05:03:18 +0200

    (tests) Updated The Tests for the Parser

--- src/test/test_parser.py
+++ src/test/test_parser.py
@@ -2,11 +2,11 @@ import pytest
 from helpers.parsing import parsing
 
 @pytest.mark.parametrize("file_number", range(1, 2))
-def test_parsing_files_1_to_5(file_number):
+def test_parsing_files_first(file_number):
     assert parsing(f"out/testing/tokens/lexer-{file_number}.xml", f"out/testing/tree/tree-{file_number}.xml") == True
 
 @pytest.mark.parametrize("file_number", range(2, 21))
-def test_parsing_files_6_to_20(file_number):
+def test_parsing_files_2_to_20(file_number):
     assert parsing(f"out/testing/tokens/lexer-{file_number}.xml", f"out/testing/tree/tree-{file_number}.xml") == False, \
         f"Parsing Should Not Pass For lexer-{file_number}.xml"
 
