fe8d86689b985c24b4b0b14ba03b908c0450ea99
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-16 04:34:57 +0200

    (tests) Using Paramerised Tests for The Parser

--- src/test/test_parser.py
+++ src/test/test_parser.py
@@ -1,15 +1,14 @@
 import pytest
-from helpers.lexing import lexing
 from helpers.parsing import parsing
 
-def test_pytest_is_working_1():
-    assert True, "Parser Default Tests Working"
+@pytest.mark.parametrize("file_number", range(1, 2))
+def test_parsing_files_1_to_5(file_number):
+    assert parsing(f"out/testing/tokens/lexer-{file_number}.xml", f"out/testing/tree/tree-{file_number}.xml") == True
 
-def test_parsing_from_files():
-    for k in range(1, 21):
-        expected = True if k <= 1 else False
-        result = parsing(f"out/testing/tokens/lexer-{k}.xml", f"out/testing/tree/tree-{k}.xml")
-        assert result == expected, f"Parsing Should Not Pass For lexer-{k}.xml"
+@pytest.mark.parametrize("file_number", range(2, 21))
+def test_parsing_files_6_to_20(file_number):
+    assert parsing(f"out/testing/tokens/lexer-{file_number}.xml", f"out/testing/tree/tree-{file_number}.xml") == False, \
+        f"Parsing Should Not Pass For lexer-{file_number}.xml"
 
 if __name__ == "__main__":
     # pytest -v
