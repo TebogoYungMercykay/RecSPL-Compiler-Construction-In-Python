fe397c068e108b781c3eabe2593f64958c034dc0
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-16 05:03:32 +0200

    (tests) Updated The Tests for the Semantics

--- src/test/test_semantics.py
+++ src/test/test_semantics.py
@@ -1,10 +1,22 @@
 import pytest
+from helpers.analyser import analyser
 
-def test_pytest_is_working_1():
-    assert 1 + 1 == 2, "Semantics Default Tests Working"
+@pytest.mark.parametrize("file_number", range(1, 2))
+def test_analyser_files_first(file_number):
+    assert analyser(
+            f"out/testing/tree/tree-{file_number}.xml",
+            f"out/testing/semantics/crawling/crawl-{file_number}.txt",
+            f"out/testing/semantics/symbols/symbols-{file_number}.txt"
+        ) == True
 
-def test_semantics_from_files():
-    assert 1 + 1 == 2, "Semantics - All Good"
+@pytest.mark.parametrize("file_number", range(2, 21))
+def test_analyser_files_2_to_20(file_number):
+    assert analyser(
+            f"out/testing/tree/tree-{file_number}.xml",
+            f"out/testing/semantics/crawling/crawl-{file_number}.txt",
+            f"out/testing/semantics/symbols/symbols-{file_number}.txt"
+        ) == True, \
+        f"Semantics Should Not Pass For Tree-{file_number}.xml"
 
 if __name__ == "__main__":
     # pytest -v
