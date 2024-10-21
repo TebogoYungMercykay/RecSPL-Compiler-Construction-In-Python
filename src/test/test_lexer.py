01482c8211820646d2d99c40a90c573ba7a0f8b7
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-21 17:35:10 +0200

    (test) Unit Tests For the Lexer

--- src/test/test_lexer.py
+++ src/test/test_lexer.py
@@ -1,6 +1,10 @@
 import pytest
 from helpers.lexing import lexing
 
+def test_pytest_is_working_1():
+    print("\033[94m\n==== LEXER: TEST CASES USING PYTEST ====\n\033[0m")
+    assert 1 + 1 == 2, "Default Test For Lexer Is Working!"
+
 @pytest.mark.parametrize("file_number", range(1, 6))
 def test_lexing_files_1_to_5(file_number):
     assert lexing(f"out/testing/tokens/lexer-{file_number}.xml", f"out/testing/recspl/code-{file_number}.txt") == True
