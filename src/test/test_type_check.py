97975926c33148f4d961ad077c35cce6b8f7aa9e
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-23 05:00:07 +0200

    (tests) PlaceHolder Test For the Type Checker

--- src/test/test_type_check.py
+++ src/test/test_type_check.py
@@ -1,4 +1,5 @@
 import pytest
+from helpers.type_checker import check_types
 
 print("\033[94m\n==== TYPE CHECKER: TEST CASES USING PYTEST ====\n\033[0m")
 
@@ -6,12 +7,12 @@ def test_pytest_is_working_1():
     assert 1 + 1 == 2, "Default Test For Type Checker Is Working!"
 
 @pytest.mark.parametrize("file_number", range(1, 2))
-def test_semantics_files_first(file_number):
-    assert file_number + file_number == 2, "Type Checker Default Tests Working"
+def test_type_check_files_first(file_number):
+    assert check_types(None, None) == False, f"{file_number}: Type Checker Default Tests Working"
 
 @pytest.mark.parametrize("file_number", range(2, 21))
-def test_semantics_files_2_to_20(file_number):
-    assert file_number + file_number == 2 * file_number, "Type Checker - All Good"
+def test_type_check_files_2_to_20(file_number):
+    assert check_types(None, None) == False, f"{file_number}: Type Checker - All Good"
 
 if __name__ == "__main__":
     # pytest -v
