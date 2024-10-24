1e1c2c8f54aec24d5ad9e2ca401abdad871c09d6
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-24 10:11:41 +0200

    (src) Main Method To Run Files In Bulk & Single Files

--- src/main.py
+++ src/main.py
@@ -1,79 +1,36 @@
-from helpers.convert_to_dfa import convert_to_dfa
-from helpers.lexing import lexing
-from helpers.parsing import parsing
-from helpers.analyser import analyser
-from helpers.type_checker import check_types 
+from runner import runner
 
-# from helpers.analyser import semantics
+def single(code_filename, lexer_filepath, parser_filepath, crawling_filepath, semantics_filepath):
+    print("\033[94m===== START ({files['code_filename']}) =====\033[0m")
+    
+    result = runner(code_filename, lexer_filepath, parser_filepath, crawling_filepath, semantics_filepath)
+    
+    print(f"\033[94m===== DONE ({result == True}) =====\n\n\033[0m")
 
-# Note 1 := The Source Code Should be witten in the file specifed in code_filename path file
-# Note 2 := Replace Filename Bellow, Eg. out/lexer.xml
-#        := This will attempt to store the XML file in out directory.
+def bulk(list_files):
+    for files in list_files:
+        print(f"\033[94m===== START ({files['code_filename']}) =====\033[0m")
+
+        result = runner(files['code_filename'], files['lexer_filepath'], files['parser_filepath'], files['crawling_filepath'], files['semantics_filepath'])
+    
+        print(f"\033[94m===== DONE ({result == True}) =====\n\n\033[0m")
 
 code_filename = "RecSPL.txt"
-
-dfa_filepath = "out/dfa_output.txt"
 lexer_filepath = "out/lexer.xml"
 parser_filepath = "out/syntax_tree.xml"
 crawling_filepath = "out/semantics_crawling_output.txt"
-semantics_filepath = "out/semantics_sybols_output.txt"
-syntax_tree = None
-symbols = None
+semantics_filepath = "out/semantics_symbols_output.txt"
+single(code_filename, lexer_filepath, parser_filepath, crawling_filepath, semantics_filepath)
 
-def test():
-    LEXING_SUCCESSFUL = lexing(lexer_filepath, code_filename)
-    if not LEXING_SUCCESSFUL:
-        print(
-            "\033[91mParsing Paused:  Please Fix Lexical Errors To Continue...\033[0m"
-        )
+L = []
+for k in range(1, 21):
+    file_paths = {
+        "code_filename": f"out/testing/typechecker/recspl/code-{k}.txt",
+        "lexer_filepath": f"out/testing/typechecker/tokens/lexer-{k}.xml",
+        "parser_filepath": f"out/testing/typechecker/tree/tree-{k}.xml",
+        "crawling_filepath": f"out/testing/typechecker/crawling/crawl-{k}.txt",
+        "semantics_filepath": f"out/testing/typechecker/symbols/symbols-{k}.txt"
+    }
+    L.append(file_paths)
 
-    syntax_tree = parsing(lexer_filepath, parser_filepath)
-    PARSING_SUCCESSFUL = (
-        (syntax_tree != None) if LEXING_SUCCESSFUL else False
-    )
-    if not PARSING_SUCCESSFUL:
-        if not LEXING_SUCCESSFUL:
-            print()
-        else:
-            print(
-                "\033[91mSemantic Analysis Paused:  Please Check for Syntax Errors...\033[0m"
-            )
-
-    symbols = analyser(parser_filepath, crawling_filepath, semantics_filepath)
-    SEMANTICS_SUCCESSFUL = (
-        (symbols != None)
-        if PARSING_SUCCESSFUL
-        else False
-    )
-    if not SEMANTICS_SUCCESSFUL:
-        if not PARSING_SUCCESSFUL:
-            print()
-        else:
-            print(
-                "\033[91mType Checker Paused:  Please Check for Function & Variable Declaration Errors...\033[0m"
-            )
-    
-    TYPE_SUCCESSFUL = (
-        check_types(syntax_tree, symbols)
-        if PARSING_SUCCESSFUL
-        else False
-    )
-    if not TYPE_SUCCESSFUL:
-        if not SEMANTICS_SUCCESSFUL:
-            print()
-        else:
-            print(
-                "\033[91mType Checker Paused:  Please Check for Type Errors...\033[0m"
-            )
-    
-    if (TYPE_SUCCESSFUL):
-        print(
-            "\033[92mTesting Complete!.\033[0m"
-        )
-    else:
-        print(
-            "\033[91mSome Tests Were Unsuccessful!.\033[0m"
-        )
-    return True
-
-test()
+bulk(L)
