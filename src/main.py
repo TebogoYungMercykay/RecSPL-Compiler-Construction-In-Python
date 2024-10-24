e63e5942feb4e70489136e517ea86b6abee26e9e
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-24 19:58:13 +0200

    (update) Added Default File Paths for Output

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from runner import runner
 
-def single(code_filename, lexer_filepath, parser_filepath, crawling_filepath, semantics_filepath):
+def single(
+    code_filename,
+    lexer_filepath = "output/lexer.xml",
+    parser_filepath = "output/syntax_tree.xml",
+    crawling_filepath = "output/semantics_crawling_output.txt",
+    semantics_filepath = "output/semantics_symbols_output.txt",
+):
     print("\033[94m===== START ({files['code_filename']}) =====\033[0m")
     
     result = runner(code_filename, lexer_filepath, parser_filepath, crawling_filepath, semantics_filepath)
     
     print(f"\033[94m===== DONE ({result == True}) =====\n\n\033[0m")
 
-def bulk(list_files):
+def bulk(list_files=[]):
     for i, files in enumerate(list_files):
         print(f"\033[94m===== START ({files['code_filename']}) =====\033[0m")
 
@@ -22,20 +28,21 @@ def bulk(list_files):
                 break
 
 code_filename = "RecSPL.txt"
-lexer_filepath = "out/lexer.xml"
-parser_filepath = "out/syntax_tree.xml"
-crawling_filepath = "out/semantics_crawling_output.txt"
-semantics_filepath = "out/semantics_symbols_output.txt"
+lexer_filepath = "output/lexer.xml"
+parser_filepath = "output/syntax_tree.xml"
+crawling_filepath = "output/semantics_crawling_output.txt"
+semantics_filepath = "output/semantics_symbols_output.txt"
 single(code_filename, lexer_filepath, parser_filepath, crawling_filepath, semantics_filepath)
 
 L = []
 for k in range(1, 21):
     file_paths = {
+        # TODO: replace code_filename with the ones you need to use
         "code_filename": f"out/testing/typechecker/recspl/code-{k}.txt",
-        "lexer_filepath": f"out/testing/typechecker/tokens/lexer-{k}.xml",
-        "parser_filepath": f"out/testing/typechecker/tree/tree-{k}.xml",
-        "crawling_filepath": f"out/testing/typechecker/crawling/crawl-{k}.txt",
-        "semantics_filepath": f"out/testing/typechecker/symbols/symbols-{k}.txt"
+        "lexer_filepath": f"output/tokens/lexer-{k}.xml",
+        "parser_filepath": f"output/tree/tree-{k}.xml",
+        "crawling_filepath": f"output/crawling/crawl-{k}.txt",
+        "semantics_filepath": f"output/symbols/symbols-{k}.txt"
     }
     L.append(file_paths)
 
